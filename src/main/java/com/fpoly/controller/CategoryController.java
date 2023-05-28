9cb033e6ec9a23b8340a372b2262a5986d859b68
Nghia Quach <huunghiaquach@gmail.com>
2023-05-28 22:05:27 +0700

    pagination with numbers and selected style

--- src/main/java/com/fpoly/controller/CategoryController.java
+++ src/main/java/com/fpoly/controller/CategoryController.java
@@ -1,10 +1,18 @@
 package com.fpoly.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fpoly.dao.CategoryDAO;
 import com.fpoly.model.Category;
@@ -16,20 +24,32 @@ public class CategoryController {
     CategoryDAO categoryDAO;
 
     @GetMapping("/category/index")
-    public String getCategories(Model model) {
+    public String getCategories(Model model,@RequestParam("p") Optional<Integer> p) {
         var category = new Category();
         model.addAttribute("category", category);
-        var categories = categoryDAO.findAll();
-        for (Category cat : categories) {
-            System.out.println(cat.getProducts().size());
-        }
+        //Sort sort = Sort.by(Direction.ASC, "name");
+        var numberOfRecords = categoryDAO.count();
+        var numberOfPages = (int) Math.ceil(numberOfRecords / 3.0);
+        model.addAttribute("numberOfPages", numberOfPages);
+        Pageable sort = PageRequest.of(p.orElse(0), 3, Sort.by("name").ascending());
+        model.addAttribute("currIndex", p.orElse(0));
+        var categories = categoryDAO.findAll(sort);
+        // for (Category cat : categories) {
+        //     System.out.println(cat.getProducts().size());
+        // }
+
         model.addAttribute("categories", categories);
             return "category/index";
+    }
+
+    @GetMapping("/category/page")
+    public String paginate(Model model,@RequestParam("p") Optional<Integer> p){
+        return this.getCategories(model, p);
     }
 
     @GetMapping("/category/save")
     public String saveCategory(@ModelAttribute("category") Category cat ,Model model) {
         categoryDAO.save(cat);
-        return this.getCategories(model);
+        return this.getCategories(model, Optional.empty());
     }
 }
